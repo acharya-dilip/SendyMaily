4190da75ddacb2c7445d096b68c4af33a1868160
JCT-Biura <88521658+JCT-B@users.noreply.github.com>
2025-10-06 12:37:30 +0545

    Implemeneted Margins for entry and label for gmail and password

--- main.c
+++ main.c
@@ -26,25 +26,54 @@ static void activate (GtkApplication *app,gpointer user_data) {
     gridParentLoginScreen = gtk_grid_new();
     gtk_window_set_child(GTK_WINDOW(windowLoginScreen),gridParentLoginScreen);
     //Margins & Paddings
-    gtk_widget_set_halign(gridParentLoginScreen, GTK_ALIGN_CENTER);
+    gtk_widget_set_halign(gridParentLoginScreen, GTK_ALIGN_START);
     gtk_widget_set_valign(gridParentLoginScreen, GTK_ALIGN_CENTER);
+
 
     //Implementation of label Gmail
     labelGmail = gtk_label_new("GMail:");
     gtk_grid_attach(GTK_GRID(gridParentLoginScreen),labelGmail,0,0,1,1);
+    //Margins & Paddings
+    gtk_widget_set_halign(labelGmail,GTK_ALIGN_END);
+    gtk_widget_set_margin_start(labelGmail,10);
+    gtk_widget_set_margin_bottom(labelGmail,10);
+    gtk_widget_set_margin_top(labelGmail,10);
+
+
+
     //Implementation of entry Gmail
     entryGmail = gtk_entry_new();
     gtk_entry_set_placeholder_text(GTK_ENTRY(entryGmail),"ENTER GMAIL");
-    gtk_grid_attach(GTK_GRID(gridParentLoginScreen),entryGmail,1,0,1,1);
+    gtk_grid_attach(GTK_GRID(gridParentLoginScreen),entryGmail,1,0,3,1);
+    //Margins & Paddings
+    gtk_widget_set_size_request(entryGmail,270,-1);
+    gtk_widget_set_margin_bottom(entryGmail,10);
+    gtk_widget_set_margin_top(entryGmail,10);
+    gtk_widget_set_margin_start(entryGmail,10);
+
+
 
     //Implementation of Label Password
     labelPassword = gtk_label_new("Pass:");
     gtk_grid_attach(GTK_GRID(gridParentLoginScreen),labelPassword,0,1,1,1);
+    //Margins & Paddings
+    gtk_widget_set_halign(labelPassword,GTK_ALIGN_END);
+    gtk_widget_set_margin_start(labelPassword,10);
+    gtk_widget_set_margin_bottom(labelPassword,10);
+
+
+
 
     //Implementation of entry Password
     entryPassword = gtk_entry_new();
     gtk_entry_set_placeholder_text(GTK_ENTRY(entryPassword),"ENTER PASSWORD");
-    gtk_grid_attach(GTK_GRID(gridParentLoginScreen),entryPassword,1,1,1,1);
+    gtk_grid_attach(GTK_GRID(gridParentLoginScreen),entryPassword,1,1,3,1);
+    //Margins & Paddings
+    gtk_widget_set_size_request(entryPassword,270,-1);
+    gtk_widget_set_margin_bottom(entryPassword,10);
+    gtk_widget_set_margin_start(entryPassword,10);
+
+
 
 
 
